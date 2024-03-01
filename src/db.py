0412e6adcd1e1c88570c36b4c9268cdb64b6df8c
Daniel Garcia Briseno <94071409+dgarciabriseno@users.noreply.github.com>
2024-03-01 15:45:49 -0500

    Add path property

--- src/db.py
+++ src/db.py
@@ -39,6 +39,10 @@ class DataRow(_Base):
     @property
     def url(self):
         return f"{HOSTNAME}{self.filepath}/{self.filename}"
+    
+    @property
+    def path(self):
+        return f"{self.filepath}/{self.filename}"
 
     def to_csv(self, columns: list[str]) -> str:
         # Special case when "Time" is the only parameter
